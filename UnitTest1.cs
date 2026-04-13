21631ef16c7a49bbf56060bce23dfeff653afa91
Crestfall <mas025@edu.zealand.dk>
2026-04-13 11:32:12 +0200

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,15 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+namespace DRMusicUITest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
