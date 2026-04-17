455ac594b4906b4822412674e2e1bd1d0476ca55
Crestfall <mas025@edu.zealand.dk>
2026-04-17 12:51:25 +0200

    test start

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -6,9 +6,31 @@ namespace DRMusicUITest
 {
     public class UnitTest1
     {
+        private static readonly string DriverDirectory = "C:\\webDrivers";
+
+       
+        string url = "https://drmusic20260417123557-fmfkb9ffd7hxgpeh.swedencentral-01.azurewebsites.net/ ";
+
         [Fact]
-        public void Test1()
+        public void GetAllTest()
         {
+            //IWebDriver driver = new ChromeDriver(DriverDirectory);
+            //IWebDriver driver = new FirefoxDriver(DriverDirectory);
+            IWebDriver driver = new EdgeDriver(DriverDirectory);
+
+            driver.Navigate().GoToUrl(url);
+
+            Assert.Equal("DRMusic", driver.Title);
+
+        
+
+            //IWebElement button = driver.FindElement(By.Id("getAllButton"));
+            //button.Click();
+
+            //IWebElement message = driver.FindElement(By.Id("recordlist"));
+            //Assert.Equal("abc ", message.Text);
+
+            driver.Dispose();
 
         }
     }
